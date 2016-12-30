1dbc59810bad32a9ac502eb808456220669b2b48
RoobinGood <a.robingood@gmail.com>
2016-12-30 07:22:55 +0300

    add test for deleteMany with error hook

--- test/deleteMany.js
+++ test/deleteMany.js
@@ -171,6 +171,50 @@ describe('Test deleteMany', function() {
 				done
 			);
 		});
+
+		it('with error hook, should be ok', function(done) {
+			var entities = [helpers.getEntity(), helpers.getEntity()],
+				condition = {
+					_id: {
+						$in: [entities[0]._id, entities[1]._id]
+					}
+				},
+				collection = helpers.getCollection({
+					beforeDeleteMany: function(params, callback) {
+						callback(new Error('Before hook error'));
+					},
+					error: function(params, callback) {
+						expect(params.condition).eql(condition);
+						expect(params.options).eql({});
+						expect(params.error).ok();
+						callback()
+					}
+				});
+			Steppy(
+				function() {
+					collection.insertMany(entities, this.slot());
+				},
+				function() {
+					collection.deleteMany(condition, this.slot());
+				},
+				function(err) {
+					expect(err).ok();
+					expect(err.message).eql('Before hook error');
+
+					Steppy(
+						function() {
+							collection.count(this.slot());
+						},
+						function(err, count) {
+							expect(count).eql(2);
+
+							helpers.cleanDb(this.slot());
+						},
+						done
+					);
+				}
+			);
+		});
 	});
 
 	after(helpers.cleanDb);
